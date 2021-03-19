c6feba26a2e0cae4ca33c9d03de193b4d4c3abbd
Christopher EVANS <christopher.evans@students.emmauscc.sa.edu.au>
2021-03-19 11:00:53 +1030

    Convert To Binary Function Done

--- convertToBinary.js
+++ convertToBinary.js
@@ -1,6 +1,18 @@
 function convertToBinary(textIn){
-
+    eightBitArray=[];
+            asciiDec = textIn.charCodeAt(i);
+            asciiDecArray.push(textIn.charCodeAt(i))
+            console.log(asciiDecArray);
+            for(var j=7;j>-1;j--){
+                if(Math.pow(2, j)<=asciiDec){
+                    eightBitArray.push(1);
+                    asciiDec=asciiDec-Math.pow(2, j);
+                }else if(Math.pow(2, j)>asciiDec){
+                    eightBitArray.push(0);
+                }
+            }
+            binaryText.push(eightBitArray);
     
-
+    console.log(binaryText);
     return binaryText
 }
