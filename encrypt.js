3dab396f181857b372d5ffee053bd64ecd784b00
Grady CLARK <grady.clark@students.emmauscc.sa.edu.au>
2021-03-19 11:05:04 +1030

    start of encrypt algorithm

--- encrypt.js
+++ encrypt.js
@@ -1,5 +1,30 @@
-function encrypt(input, key, type){
-    
-    
+
+var binary=[0,1,0,0,1,1,0,0];
+var binaryShift=[];
+
+
+function encrypt(input, key){
+
+    if( binary[binary.length] == 0){
+
+        binary[binary.length] == 1;
+
+    }else if( binary[binary.length] == 1){
+
+        if( binary[binary.length-1] == 0){
+
+            binary[binary.length-1] == 1;
+
+            binary[binary.length] == 0;
+
+        }else if( binary[binary.length] == )
+
+    }
+
     return output
+
+}
+
+for(var i=0;i<binary.length;i++){
+    binaryShift.push(encrypt(binary[i],1));
 }
