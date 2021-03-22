1c356292f959eb7acc76ed6a0ff12e2588cfbfd0
Samuel FURLONG <samuel.furlong@students.emmauscc.sa.edu.au>
2021-03-22 14:28:04 +1030

    XOR Gate 2.1

--- XORgate.js
+++ XORgate.js
@@ -1,18 +1,24 @@
 // IV needs to be set within a loop, initally from the first binary word. Then from then on the output of Grady's function.
 
-var character = [1,1,0,1,1,0,0,0];
-var IV = [0,1,1,0,1,0,1,0]; //input needs to be an array as such.
+var a = [1,1,0,1,1,0,0,0];
+var b = [0,1,1,0,1,0,1,0]; //input needs to be an array as such.
 var output = [];
 
 //XORgate(character,IV);
 
-function XORgate(a,b){
-    for (x=0; x<8; x++){
-        if (a[x]==b[x]) {
-            output.push(0);
-        } else {
-            output.push(1);
+function XORgate(character,IV,gateType) {
+    if (gateType==1) {
+        for (x=0; x<8; x++){
+            if (character[x]==IV[x]) {
+                output.push(0);
+            } else {
+                output.push(1);
+            }
         }
+    } else if (gateType==2) {
+
+    } else if (gateType==3) {
+
     }
 
     console.log(output);
